3c4dabfd7587e4a09f1ff1a8dfc984f754ac337e
FrostingRaven <ravenfrost@abv.bg>
2022-06-10 19:17:59 +0300

    LevelTransitions And start and end Scenes

---
+++ Whip-Hero/Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelEnd : MonoBehaviour
+{
+    public Animator animator;
+    public LayerMask End;
+    private float transitionTime=1f;
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.layer==End){
+            LoadNextLevel(1);
+        }
+    }
+    public void LoadNextLevel(int v){
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex+v));
+    }
+
+    IEnumerator LoadLevel(int levelIndex){
+        animator.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
